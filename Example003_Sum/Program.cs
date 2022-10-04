8b24cb2a9bb737688c812a22a0684a4732771b64
Andrey Sorokin <ands@list.ru>
2022-10-05 02:01:49 +0400

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(5,50); //5,6,7,8,...49
+Console.WriteLine(numberA);
+int numberB = new Random().Next(100,110); // 100,101,...109
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
